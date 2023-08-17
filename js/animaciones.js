f175486073fca3c6fcb593ed0534442238a78e34
“XimitlAtlixco” <yaeljuarezbarranco1@gmail.com>
2023-08-16 18:14:09 -0600

    ajua

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,19 +1,25 @@
+import anime from '../node_modules/animejs/lib/anime.es.js';
+
 var list
 var listItem
 var x=0
+var fondo
 
 function cargar(){
      list=document.getElementById("slider-ul")
      listItem=document.getElementsByClassName("classListSlider");
      console.log(listItem[0].clientWidth)
+     fondo=document.getElementsByTagName("body");
 }
 document.addEventListener("DOMContentLoaded",cargar())
- 
+ console.log(fondo);
 
 function regresar(){
     list.style.transform='translateX(0px)'
     x=0
 }
+
+
 
 setInterval(()=>{
 if(x!=listItem.length-1){
@@ -24,5 +30,4 @@ else{
     regresar()
 }
 },5000)
-
 
